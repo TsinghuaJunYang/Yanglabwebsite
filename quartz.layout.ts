122141113d720d5d3caf5bf40fbe815415ee724a
TsinghuaJunYang <larix@tsinghua.edu.cn>
2026-07-29 09:32:30 +0800

    Quartz sync: Jul 29, 2026, 9:32 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,7 +38,9 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+		filterFn: (node) => node.name !=="Materials",
+	}),
   ],
   right: [
     Component.Graph(),
@@ -63,7 +65,9 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+		filterFn: (node) => node.name !=="Materials",
+	}),
   ],
   right: [],
 }
